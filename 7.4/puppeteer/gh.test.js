b3fe1ca40b831c0e2f0a49163dd51d5d643ec641
Victroria Anisimova <vitkakim@mail.ru>
2022-11-14 19:28:21 +0300

    third commit

--- 7.4/puppeteer/gh.test.js
+++ 7.4/puppeteer/gh.test.js
@@ -2,7 +2,6 @@ let page;
 
 beforeEach(async () => {
   page = await browser.newPage();
-  await page.goto("https://github.com/team");
 }, 5000);
 
 afterEach(() => {
@@ -11,6 +10,10 @@ afterEach(() => {
 
 describe("Github page tests", () => {
   
+  beforeEach(async () => {
+    await page.goto("https://github.com/team");
+  }, 5000);
+
   test("The h1 header content'", async () => {
     const firstLink = await page.$("header div div a");
     await firstLink.click();
